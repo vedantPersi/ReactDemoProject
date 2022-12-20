0fac20a25d4c5cdc9ffe76cf3e4bd19201d31d95
jabed-Oyesters <jabed@oyesters.in>
2022-12-20 23:14:54 +0600

    Jabed - Modified the home page UI

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,78 +1,56 @@
-import React, { useContext } from "react";
-import Modal from "../CommonComponents/Modal";
-import CartItem from "./CartItem";
-import CartContext from "../../store/cart-context";
+import React, {useContext} from 'react';
+import Modal from '../CommonComponents/Modal';
+import CartItem from './CartItem';
+import CartContext from '../../store/cart-context';
 
 type cartFunction = {
-  onCloseCart: () => void;
-};
+  onCloseCart: () => void
+}
 
-const Cart = (props: cartFunction) => {
+const Cart = (props:cartFunction) => {
   const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(
-    2
-  )}`;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id: any) => {
+  
+  const cartItemRemoveHandler = (id:any) => {
     cartCtx.removeItem(id);
   };
 
-  const cartItemAddHandler = (item: any) => {
-    cartCtx.addItem({ ...item, amount: 1 });
+  const cartItemAddHandler = (item:any) => {
+    cartCtx.addItem({...item, amount: 1});
   };
 
   const cartItems = (
     <ul className="m-0 p-0">
-      {cartCtx.items.map((item: any) => (
+      {cartCtx.items.map((item:any) => (
         <CartItem
           key={item.id}
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove={cartItemRemoveHandler.bind(
-            null,
-            item.id
-          )}
-          onAdd={cartItemAddHandler.bind(
-            null,
-            item
-          )}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
   );
 
   return (
-    //https://stackoverflow.com/questions/55129942/typescript-styled-component-error-type-children-string-has-no-properti
-    <div className="absolute mt-[100px]">
-      {" "}
-      <div>
-        {" "}
-        <Modal onClose={props.onCloseCart}>
-          {cartItems}
-          <div className="flex justify-between items-center font-bold text-2xl">
-            <span>Total Amount</span>
-            <span>{totalAmount}</span>
-          </div>
-          <div className="text-right">
-            <button
-              className="text-orange-900"
-              onClick={props.onCloseCart}
-            >
-              Close
-            </button>
-            {hasItems && (
-              <button className="cursor-pointer bg-transparent ml-4">
-                Order
-              </button>
-            )}
-          </div>
-        </Modal>
+//https://stackoverflow.com/questions/55129942/typescript-styled-component-error-type-children-string-has-no-properti
+    <Modal onClose={props.onCloseCart}>
+      {cartItems}
+      <div className="flex justify-between items-center font-bold text-2xl">
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
-    </div>
+      <div className="text-right">
+        <button className="text-orange-900" onClick={props.onCloseCart}>Close</button>
+        {hasItems && <button className="cursor-pointer bg-transparent ml-4">Order</button>}
+      </div>
+    </Modal>
   );
-};
+}
 
 export default Cart;
